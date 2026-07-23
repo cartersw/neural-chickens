8ac0ee18b696357c0492b373736452fcdd204432
cartersw <carterwildenradt@gmail.com>
2026-07-23 01:17:33 -0400

    reformat navbar

--- web/src/app/components/Navbar.tsx
+++ web/src/app/components/Navbar.tsx
@@ -4,18 +4,27 @@ import { RiNextjsFill } from 'react-icons/ri'
 
 const Navbar = () => {
     return (
-        <nav className='sticky top-0 w-full flex items-center justify-around py-5 px-24 border
-        -b border-gray-700 bg-black'>
-
-        <Link href="/" className="text-gray-300 hover:text-white transition-colors">Home</Link>
-
-            <ul className="flex gap-10 text-lg">
-                <Link href="/about" className="text-gray-300 hover:text-white transition-colors">About</Link>
-                <Link href="/test" className="text-gray-300 hover:text-white transition-colors">Test</Link>
-            </ul>
+        <nav className="fixed top-6 left-1/2 -translate-x-1/2 z-50 justify-center">
+            <div className="border-b-2 border-gray-700 px-8 py-3">
+                <ul className="flex items-center gap-10 text-lg text-black">
+                    <li>
+                        <Link href="/" className="hover:text-gray-300 transition-colors">
+                        Race
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/chickens" className="hover:text-gray-300 transition-colors">
+                        Chickens
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/about" className="hover:text-gray-300 transition-colors">
+                        About
+                        </Link>
+                    </li>
+                </ul>
+            </div>
         </nav>
-
-        
     )
 }
 
