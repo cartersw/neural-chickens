b4ee364ceb89b00e04cf4260f194e0fb84118829
cartersw <carterwildenradt@gmail.com>
2026-07-22 23:47:08 -0400

    remove logo and sticky navbar

--- web/src/app/components/Navbar.tsx
+++ web/src/app/components/Navbar.tsx
@@ -4,12 +4,10 @@ import { RiNextjsFill } from 'react-icons/ri'
 
 const Navbar = () => {
     return (
-        <nav className='fixed top-0 w-full flex items-center justify-around py-5 px-24 border
+        <nav className='sticky top-0 w-full flex items-center justify-around py-5 px-24 border
         -b border-gray-700 bg-black'>
 
-        <Link href="/" className="transition duration-300 hover:scale-110">
-            <RiNextjsFill className="w-16 h-16"></RiNextjsFill>
-            </Link>
+        <Link href="/" className="text-gray-300 hover:text-white transition-colors">Home</Link>
 
             <ul className="flex gap-10 text-lg">
                 <Link href="/about" className="text-gray-300 hover:text-white transition-colors">About</Link>
