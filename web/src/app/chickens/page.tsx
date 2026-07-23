a81cb9b8112c28ef2eec2ca001fc4579437bbff4
cartersw <carterwildenradt@gmail.com>
2026-07-23 01:29:35 -0400

    make gif smaller

--- web/src/app/chickens/page.tsx
+++ web/src/app/chickens/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
     return(
         <main className="min-h-screen flex items-center justify-center pt-24">
-            <div className="w-[80%] max-w-5xl rounded-2xl border border-gray-700 bg-gray-900 p-3">
+            <div className="w-[80%] max-w-2xl rounded-2xl border border-gray-700 bg-gray-900 p-3">
                 <img src="/burger.gif"
                 alt="guy eating burger"
                 className="w-full h-auto rounded-xl"
