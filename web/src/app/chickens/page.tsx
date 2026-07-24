443b4415e412e060c57177329f400629c201b20e
cartersw <carterwildenradt@gmail.com>
2026-07-24 04:09:54 -0400

    change colors and formatting chickens page

--- web/src/app/chickens/page.tsx
+++ web/src/app/chickens/page.tsx
@@ -2,18 +2,16 @@ import React from 'react'
 
 const page = () => {
     return(
-        <main className="min-h-screen flex items-center justify-center pt-24">
-            <div className="flex gap-8 p-5">
-                <div className="rounded-2xl border border-gray-700 bg-gray-900 p-3">
+        <main className="min-h-screen flex items-center justify-center pt-18">
+            <div className="flex justify-center gap-8 w-[90%] p-5">
+                <div className="w-[35%] rounded-2xl border-4 border-olive-900 bg-olive-700 p-1">
                     <img src="/burger.gif"
                     alt="guy eating burger"
                     className="w-full h-auto rounded-xl"
                     />
                 </div>
-                <div className="w-40 bg-gray-900 rounded-xl">
+                <div className="w-60 rounded-2xl flex flex-col items-center justify-evenly border-4 border-olive-900 bg-olive-700">
                     <div className=""></div>
-                    
-
                 </div>
             </div>
         </main>
