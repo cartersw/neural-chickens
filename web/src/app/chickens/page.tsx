66db9978af9c5645205f3db6a88baa1567c58594
cartersw <carterwildenradt@gmail.com>
2026-07-23 23:37:17 -0400

    gif now has sidebar, refactored

--- web/src/app/chickens/page.tsx
+++ web/src/app/chickens/page.tsx
@@ -3,11 +3,18 @@ import React from 'react'
 const page = () => {
     return(
         <main className="min-h-screen flex items-center justify-center pt-24">
-            <div className="w-[80%] max-w-2xl rounded-2xl border border-gray-700 bg-gray-900 p-3">
-                <img src="/burger.gif"
-                alt="guy eating burger"
-                className="w-full h-auto rounded-xl"
-                />
+            <div className="flex gap-8 p-5">
+                <div className="rounded-2xl border border-gray-700 bg-gray-900 p-3">
+                    <img src="/burger.gif"
+                    alt="guy eating burger"
+                    className="w-full h-auto rounded-xl"
+                    />
+                </div>
+                <div className="w-40 bg-gray-900 rounded-xl">
+                    <div className=""></div>
+                    
+
+                </div>
             </div>
         </main>
     )
