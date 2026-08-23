4acba0adc3b521c26320126d2ba7470f5268938b
cartersw <carterwildenradt@gmail.com>
2026-08-23 04:21:02 -0400

    Move entity classes to entities folder

--- backend/NeuralChickens.Api.Domain/Entities/SimulationChicken.cs
+++ backend/NeuralChickens.Api.Domain/Entities/SimulationChicken.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace NeuralChickens.Api.Domain
+namespace NeuralChickens.Api.Domain.Entities
 {
     public class SimulationChicken
     {
