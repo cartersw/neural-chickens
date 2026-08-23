b257b4d3975318d2fe0116aed2840fc53241b680
cartersw <carterwildenradt@gmail.com>
2026-08-23 04:19:30 -0400

    Simulation entity class

--- backend/NeuralChickens.Api.Domain/Simulation.cs
+++ backend/NeuralChickens.Api.Domain/Simulation.cs
@@ -11,5 +11,13 @@ namespace NeuralChickens.Api.Domain
         public int Id { get; set; }
         [Required]
         public SimulationType SimulationType { get; set; }
+        [Required]
+        public SimulationStatus SimulationStatus { get; set; }
+
+        public DateTime RequestedAt { get; set; }
+        public DateTime? StartedAt { get; set; }
+        public DateTime? CompletedAt { get; set; }
+        public string VideoPath { get; set; } = string.Empty;
+        
     }
 }
