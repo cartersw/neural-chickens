b1bc58cb18b2dd0c76758d19fef8ad3d97e55028
cartersw <carterwildenradt@gmail.com>
2026-08-27 01:10:53 -0400

    Add FindSimulationConfiguration + FindSimulationResult

---
+++ backend/NeuralChickens.Api.Domain/Entities/FindSimulationConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace NeuralChickens.Api.Domain.Entities
+{
+    public class FindSimulationConfiguration
+    {
+        public int SimulationId { get; set; }
+
+        public Simulation Simulation { get; set; } = null!;
+
+        [Required]
+        public float Speed { get; set; } 
+    }
+}
---
+++ backend/NeuralChickens.Api.Domain/Entities/FindSimulationResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuralChickens.Api.Domain.Entities
+{
+    public class FindSimulationResult
+    {
+        public int SimulationId { get; set; }
+
+        public Simulation Simulation { get; set; } = null!;
+
+        public int Wins { get; set; }
+    }
+}
