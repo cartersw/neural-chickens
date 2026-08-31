e51c2dd2082f502a242ceb26af8a78f5b9b11e52
cartersw <carterwildenradt@gmail.com>
2026-08-31 07:42:11 -0400

    Remove some placeholders

--- web/src/app/about/page.tsx
+++ web/src/app/about/page.tsx
@@ -4,7 +4,7 @@ const page = () => {
     return(
         <main className="min-h-screen flex items-center justify-center pt-24">
             <div className="flex gap-8 p-5 text-3xl text-black font-mono">
-                This is the about section of the application
+                WIP
             </div>
         </main>
     )
--- web/src/app/chickens/page.tsx
+++ web/src/app/chickens/page.tsx
@@ -8,10 +8,7 @@ const page = () => {
         <main className="min-h-screen flex items-center justify-center pt-18">
             <div className="flex justify-center gap-8 w-[90%] p-5">
                 <div className="w-[35%] rounded-2xl border-4 border-olive-900 bg-olive-700 p-1">
-                    <img src="/burger.gif"
-                    alt="guy eating burger"
-                    className="w-full h-auto rounded-xl"
-                    />
+                    
                 </div>
                 <div className="w-60 rounded-2xl flex flex-col items-center justify-evenly border-4 border-olive-900 bg-olive-700">
                     <div className=""></div>
