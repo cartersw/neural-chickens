41761b29e4f3fbbca24d42743e60b2d7c4914f29
cartersw <carterwildenradt@gmail.com>
2026-09-20 23:25:06 -0400

    Configure program.cs TrainingWorker

--- backend/NeuralChickens.TrainingWorker/Program.cs
+++ backend/NeuralChickens.TrainingWorker/Program.cs
@@ -1,6 +1,40 @@
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using NeuralChickens.Api.Domain;
 using NeuralChickens.TrainingWorker;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+
+var connectionString = builder.Configuration.GetConnectionString("NeuralChickensLocalDb");
+
+var connectionBuilder = new SqlConnectionStringBuilder(connectionString);
+
+if (builder.Environment.IsEnvironment("Testing"))
+{
+    connectionBuilder.InitialCatalog = "NeuralChickensTestDb";
+}
+
+builder.Services.AddDbContextPool<NeuralChickensDbContext>(options =>
+{
+    options.UseSqlServer(connectionBuilder.ConnectionString, sqlOptions =>
+    {
+        sqlOptions.MigrationsAssembly("NeuralChickens.Api.Domain");
+        sqlOptions.CommandTimeout(30);
+        sqlOptions.EnableRetryOnFailure(
+            maxRetryCount: 3,
+            maxRetryDelay: TimeSpan.FromSeconds(5),
+            errorNumbersToAdd: null
+            );
+    });
+    if (builder.Environment.IsDevelopment())
+    {
+        options.EnableSensitiveDataLogging();
+        options.EnableDetailedErrors();
+    }
+}, poolSize: 128);
+
+
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
